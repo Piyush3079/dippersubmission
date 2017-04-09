b59e191c606f8e8b68c04a146438c21f10bb44f9
Piyush Vijay <piyushvijay.1997@gmail.com>
2017-04-09 06:30:57 +0530

    routes js added

--- js/script.js
+++ js/script.js
@@ -9,9 +9,51 @@ function myMap() {
 				zoom:5,
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
+			var directionsService = new google.maps.DirectionsService;
+        	var directionsDisplay = new google.maps.DirectionsRenderer;        	
 			var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+			directionsDisplay.setMap(map);
 			var submit = document.getElementById('submit');
 			submit.addEventListener('click', function(){
-				alert("piyush");
+				calculateAndDisplayRoute(directionsService, directionsDisplay);
 			});
 		}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var waypts = [];
+        var checkboxArray = document.getElementById('pass_through_select');
+        for (var i = 0; i < checkboxArray.length; i++) {
+          if (checkboxArray.options[i].selected) {
+            waypts.push({
+              location: checkboxArray[i].value,
+              stopover: true
+            });
+          }
+        }
+
+        directionsService.route({
+          origin: document.getElementById('start_select').value,
+          destination: document.getElementById('end_point_select').value,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('direction_panel');
+            summaryPanel.innerHTML = '';
+            // For each route, display summary information.
+            for (var i = 0; i < route.legs.length; i++) {
+              var routeSegment = i + 1;
+              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                  '</b><br>';
+              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            }
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
