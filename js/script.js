87e24b97c85e69843d7209c5988a9ff1c9b3d281
Piyush Vijay <piyushvijay.1997@gmail.com>
2017-04-12 22:29:51 +0530

    epoly.js added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
+var geocoder;
+var markers = [];
+var lastVertex = 1;
+var lengthInMeters;
+var delayer;
+var index  = -1;
+var image = ['img/delivery-logistics-renault-vehicle-front-truck-icon.png',
+             'http://www.iconsplace.com/icons/preview/purple/truck-32.png',
+             'http://www.iconsplace.com/icons/preview/pink/truck-32.png',
+            ]
+var Colors = ["#FF0000", "#00FF00", "#0000FF"];
+infowindow = null;
+
 function myMap() {
-			var contentString = '';
-			infoWindow = new google.maps.InfoWindow({
-				size: new google.maps.Size(100,30),
-			});
-			var geocoder = new google.maps.Geocoder()
-			var mapProp= {
-				center:new google.maps.LatLng(20.5937,78.9629),
-				zoom:5,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-			var directionsService = new google.maps.DirectionsService;
-        	var directionsDisplay = new google.maps.DirectionsRenderer;        	
-			var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-			directionsDisplay.setMap(map);
-			var submit = document.getElementById('submit');
-			submit.addEventListener('click', function(){
-				calculateAndDisplayRoute(directionsService, directionsDisplay);
-			});
+          infowindow = new google.maps.InfoWindow(
+        { 
+          size: new google.maps.Size(150,50)
+        });
+            geocoder = new google.maps.Geocoder();
+            myOptions = {
+                zoom: 5,
+                center: {lat: 22.946557, lng: 79.194353},
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            map = new google.maps.Map(document.getElementById('googleMap'), myOptions),
+            // Instantiate a directions service.
+            directionsService = new google.maps.DirectionsService;
+              document.getElementById('submit').addEventListener('click', function() {
+        calculateAndDisplayRoute(directionsService);
+      });
 		}
+
+function createMarker(latlng, label, html) {
+// alert("createMarker("+latlng+","+label+","+html+","+color+")");
+    var contentString = '<b>'+label+'</b><br>'+html;
+    
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: label,
+        zIndex: Math.round(latlng.lat()*-100000)<<5,
+        icon: image[index]
+        });
+        marker.myname = label;
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(contentString); 
+        infowindow.open(map,marker);
+        });
+    return marker;
+} 
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         var waypts = [];
@@ -39,8 +71,60 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
           travelMode: 'DRIVING'
         }, function(response, status) {
           if (status === 'OK') {
-            directionsDisplay.setDirections(response);
+              var rendererOptions = {
+              map: map,
+              suppressMarkers : true,
+              preserveViewport: true
+            }
+          	 disp = new google.maps.DirectionsRenderer(rendererOptions);     
+              disp.setMap(map);
+              disp.setDirections(response);
+            var bounds = new google.maps.LatLngBounds();
             var route = response.routes[0];
+            index++;
+             startLocation = new Object();
+        endLocation = new Object();
+        
+        
+        
+            poly = new google.maps.Polyline({
+            path: [],
+            strokeColor: Colors[index],
+            strokeWeight: 3
+            });
+        
+      //path and legs
+        var path = response.routes[0].overview_path;
+        var legs = response.routes[0].legs;
+    
+
+        //markers
+        for (i=0;i<legs.length;i++) {
+              if (i == 0) { 
+                startLocation.latlng = legs[i].start_location;
+                startLocation.address = legs[i].start_address;
+                marker = createMarker(legs[i].start_location,"start",legs[i].start_address,Colors[index]);
+              }
+              endLocation.latlng = legs[i].end_location;
+              endLocation.address = legs[i].end_address;
+              var steps = legs[i].steps;
+
+              for (j=0;j<steps.length;j++) {
+                var nextSegment = steps[j].path;                
+                var nextSegment = steps[j].path;
+
+                for (k=0;k<nextSegment.length;k++) {
+                    poly.getPath().push(nextSegment[k]);
+                }
+
+              }
+            }
+        poly.setMap(map);
+        lengthInMeters = google.maps.geometry.spherical.computeLength(poly.getPath());
+        map.setCenter(poly.getPath().getAt(0));
+        poly2 = new google.maps.Polyline({path: [poly.getPath().getAt(0)], strokeColor:Colors[index], strokeWeight:3});
+        if (delayer) clearTimeout(delayer);
+        delayer = setTimeout(animate(lengthInMeters*0.001),2000);
             var summaryPanel = document.getElementById('direction_panel');
             summaryPanel.innerHTML = '';
             // For each route, display summary information.
@@ -57,3 +141,32 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
           }
         });
       }
+
+
+ function updatePoly(dist){
+ // Spawn a new polyline every 20 vertices, because updating a 100-vertex poly is too slow
+    if (poly2.getPath().getLength() > 20) {
+          poly2=new google.maps.Polyline([poly.getPath().getAt(lastVertex-1)]);
+          // map.addOverlay(poly2)
+        }
+
+    if (poly.GetIndexAtDistance(dist) < lastVertex+2) {
+        if (poly2.getPath().getLength()>1) {
+            poly2.getPath().removeAt(poly2.getPath().getLength()-1)
+        }
+            poly2.getPath().insertAt(poly2.getPath().getLength(),poly.GetPointAtDistance(dist));
+    } else {
+        poly2.getPath().insertAt(poly2.getPath().getLength(),endLocation.latlng);
+    }
+     
+  }
+function animate(d) {
+   if (d>lengthInMeters) {
+      marker.setPosition(endLocation.latlng);
+      return;
+   }
+    var p = poly.GetPointAtDistance(d);
+    marker.setPosition(p);
+    updatePoly(d);
+    delayer = setTimeout("animate("+(d+lengthInMeters*0.001)+")", 100);
+}
