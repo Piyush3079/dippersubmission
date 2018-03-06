fe2f4ae7041ca886100065cebad0b77a778e7eac
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-03-07 04:45:15 +0530

    route to show searched tweets

--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,8 @@ router.get('/streams/all/:init', getTweets.all_tweets);
 
 router.get('/stream/:string/:init', getTweets.tweet_string);
 
+router.get('/search/:string/:init', getTweets.tweet_string_search);
+
 router.get('/user/:id/:user_type', getTweets.tweet_user);
 
 router.get('/users/:table/:init', getTweets.get_all_users);
