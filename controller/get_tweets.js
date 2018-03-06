12aa74a3a6ac2b387e8ea9ee80649747d0d0d2ef
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-03-06 23:48:32 +0530

    tweet controller

--- controller/get_tweets.js
+++ controller/get_tweets.js
@@ -2,6 +2,8 @@ var conn = require('../routes/conn');
 
 var Twitter = require('twitter');
 
+var conn = require('../routes/conn');
+
 exports.get_tweets = function(req, res){
     var string = req.body.string;
     var id = conn.escape(req.body.id);
@@ -19,7 +21,43 @@ exports.get_tweets = function(req, res){
                 });
                 client.stream('statuses/filter', {track: string},  function(stream) {
                     stream.on('data', function(tweet) {
-                      console.log(tweet);
+                        var retweeted_status = tweet.retweet_status != false ? tweet.retweeted_status.id : 0 ;
+
+                        var query = `INSERT INTO tweets (tw_id, text, id_user, timestamp, retweeted) VALUES (${tweet.id}, ${tweet.text}, ${tweet.user.id}, ${tweet.timestamp}, ${retweeted_status})`;
+                        conn.query(query, function(err, result){
+                            if(err){
+                                console.log(err);
+                            }else{
+                                if(result.affectedRows == 1){
+                                    var query1 = `INSERT INTO tweet_user (twu_id, name, screen_name, url, description, followers_count, friends_count, status_count) VALUES (${tweet.user.id}, ${tweet.user.name}, ${tweet.user.screen_name}, ${tweet.user.url}, ${tweet.user.description}, ${tweet.user.followers_count}, ${tweet.user.friends_count}, ${tweet.user.status_count})`;
+                                    conn.query(query1, function(err, result){
+                                        if(err){
+                                            console.log(err);
+                                        }else{
+                                            if(result.affectedRows == 1){
+                                                console.log('done');
+                                            }else{
+                                                console.log('error in tweet_user');
+                                            }
+                                        }
+                                    });
+                                    var query2 = `INSERT INTO retweeted_user (rtu_id, name, screen_name, url, description, followers_count, friends_count, status_count) VALUES (${tweet.retweeted_status.user.id}, ${tweet.retweeted_status.user.name}, ${tweet.retweeted_status.user.screen_name}, ${tweet.retweeted_status.user.url}, ${tweet.retweeted_status.user.description}, ${tweet.retweeted_status.user.followers_count}, ${tweet.retweeted_status.user.friends_count}, ${tweet.retweeted_status.user.status_count})`;
+                                    conn.query(query2, function(err, result){
+                                        if(err){
+                                            console.log(err);
+                                        }else{
+                                            if(result.affectedRows == 1){
+                                                console.log('done');
+                                            }else{
+                                                console.log('Error in retweet user');
+                                            }
+                                        }
+                                    })
+                                }else{
+                                    console.log('error in tweet');
+                                }
+                            }
+                        });
                     });
                   
                     stream.on('error', function(error) {
