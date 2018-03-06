dd3ac25c62430b743a39e7b634e72a39e08a4ce6
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-03-07 03:21:07 +0530

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,59 @@
-CREATE TABLE `test2`.`user` ( `id` INT(6) NOT NULL AUTO_INCREMENT , `t_id` BIGINT(12) NOT NULL , `token` VARCHAR(255) NOT NULL , `token_secret` VARCHAR(255) NOT NULL , `screen_name` VARCHAR(255) NOT NULL , `created_at` TIMESTAMP NOT NULL , PRIMARY KEY (`id`), INDEX `twitte_id` (`t_id`)) ENGINE = InnoDB;
+CREATE TABLE `tweets` (
+  `id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `tw_id` bigint(19) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `id_user` bigint(19) NOT NULL,
+  `timestamp` varchar(255) NOT NULL,
+  `retweeted` bigint(19) NOT NULL,
+  `string` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2871 DEFAULT CHARSET=utf8
 
-CREATE TABLE `test2`.`tweets` ( `id` BIGINT(19) NOT NULL AUTO_INCREMENT , `tw_id` BIGINT(19) NOT NULL , `text` VARCHAR(255) NOT NULL , `id_user` BIGINT(19) NOT NULL , `timestamp` VARCHAR(255) NOT NULL , `retweeted` BIGINT(19) NOT NULL , `created_at` TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `user` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `t_id` bigint(12) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `token_secret` varchar(255) NOT NULL,
+  `screen_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `twitte_id` (`t_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 
-CREATE TABLE `test2`.`tweet_user` ( `id` BIGINT(19) NOT NULL AUTO_INCREMENT , `twu_id` BIGINT(19) NOT NULL UNIQUE , `name` VARCHAR(255) NOT NULL , `screen_name` VARCHAR(255) NOT NULL , `url` VARCHAR(255) NOT NULL , `desciption` VARCHAR(255) NOT NULL , `followers_count` INT(10) NOT NULL , `friends_count` INT(10) NOT NULL , `status_count` INT(10) NOT NULL , `created_at` TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `query` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(19) NOT NULL,
+  `string` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `string_search` (`string`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
 
-CREATE TABLE `test2`.`rtweeted_user` ( `id` BIGINT(19) NOT NULL AUTO_INCREMENT , `rtu_id` BIGINT(19) NOT NULL UNIQUE , `name` VARCHAR(255) NOT NULL , `screen_name` VARCHAR(255) NOT NULL , `url` VARCHAR(255) NOT NULL , `description` VARCHAR(255) NOT NULL , `followers_count` INT(10) NOT NULL , `friends_count` INT(10) NOT NULL , `status_count` INT(10) NOT NULL , `created_at` TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `tweet_user` (
+  `id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `twu_id` bigint(19) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `screen_name` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `followers_count` int(10) DEFAULT NULL,
+  `friends_count` int(10) DEFAULT NULL,
+  `status_count` int(10) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `twu_id` (`twu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2673 DEFAULT CHARSET=utf8
 
-CREATE TABLE `test2`.`query` ( `id` INT(6) NOT NULL AUTO_INCREMENT , `user_id` BIGINT(19) NOT NULL , `stirng` TEXT NOT NULL , `created_at` TIMESTAMP NOT NULL , PRIMARY KEY (`id`), FULLTEXT `string_search` (`stirng`)) ENGINE = InnoDB;
+CREATE TABLE `rtweeted_user` (
+  `id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `rtu_id` bigint(19) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `screen_name` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `followers_count` int(10) DEFAULT NULL,
+  `friends_count` int(10) DEFAULT NULL,
+  `status_count` int(10) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rtu_id` (`rtu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1284 DEFAULT CHARSET=utf8
